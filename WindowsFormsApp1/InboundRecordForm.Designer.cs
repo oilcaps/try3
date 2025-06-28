9dcd269782921d924f6fbae0a947f7039b04517f
oilcaps <240053257@stu.vtc.edu.hk>
2025-06-28 20:34:26 +0800

    123

---
+++ WindowsFormsApp1/InboundRecordForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace InventoryApp
+{
+    partial class InboundRecordForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvInbound;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label lblSupplierID;
+        private System.Windows.Forms.TextBox txtSupplierID;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvInbound = new System.Windows.Forms.DataGridView();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblSupplierID = new System.Windows.Forms.Label();
+            this.txtSupplierID = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInbound)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInbound
+            // 
+            this.dgvInbound.ColumnHeadersHeight = 29;
+            this.dgvInbound.Location = new System.Drawing.Point(20, 20);
+            this.dgvInbound.Name = "dgvInbound";
+            this.dgvInbound.ReadOnly = true;
+            this.dgvInbound.RowHeadersWidth = 51;
+            this.dgvInbound.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvInbound.Size = new System.Drawing.Size(600, 200);
+            this.dgvInbound.TabIndex = 0;
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.Location = new System.Drawing.Point(20, 230);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(80, 23);
+            this.lblProductID.TabIndex = 1;
+            this.lblProductID.Text = "Product ID:";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(100, 230);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(150, 25);
+            this.txtProductID.TabIndex = 2;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.Location = new System.Drawing.Point(20, 260);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(80, 23);
+            this.lblQuantity.TabIndex = 3;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(100, 260);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(150, 25);
+            this.txtQuantity.TabIndex = 4;
+            // 
+            // lblDate
+            // 
+            this.lblDate.Location = new System.Drawing.Point(300, 230);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(80, 23);
+            this.lblDate.TabIndex = 5;
+            this.lblDate.Text = "Date:";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(380, 229);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 25);
+            this.dtpDate.TabIndex = 6;
+            // 
+            // lblSupplierID
+            // 
+            this.lblSupplierID.Location = new System.Drawing.Point(300, 260);
+            this.lblSupplierID.Name = "lblSupplierID";
+            this.lblSupplierID.Size = new System.Drawing.Size(80, 23);
+            this.lblSupplierID.TabIndex = 7;
+            this.lblSupplierID.Text = "Supplier ID:";
+            // 
+            // txtSupplierID
+            // 
+            this.txtSupplierID.Location = new System.Drawing.Point(380, 260);
+            this.txtSupplierID.Name = "txtSupplierID";
+            this.txtSupplierID.Size = new System.Drawing.Size(150, 25);
+            this.txtSupplierID.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(100, 300);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 30);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(200, 300);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(80, 30);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(300, 300);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 30);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(400, 300);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 30);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // InboundRecordForm
+            // 
+            this.ClientSize = new System.Drawing.Size(650, 360);
+            this.Controls.Add(this.dgvInbound);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.lblSupplierID);
+            this.Controls.Add(this.txtSupplierID);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Name = "InboundRecordForm";
+            this.Text = "Inbound Record";
+            this.Load += new System.EventHandler(this.InboundRecordForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInbound)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
