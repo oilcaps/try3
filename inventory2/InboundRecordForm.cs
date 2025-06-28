b4dccaa943b45eb9448f384cb752afc986987bb9
oilcaps <240053257@stu.vtc.edu.hk>
2025-06-28 21:00:24 +0800

    try

---
+++ inventory2/InboundRecordForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory2
+{
+    public partial class InboundRecordForm : Form
+    {
+        public InboundRecordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InboundRecordForm_Load(object sender, EventArgs e) { }
+
+        private void btnAdd_Click(object sender, EventArgs e) { }
+
+        private void btnUpdate_Click(object sender, EventArgs e) { }
+
+        private void btnDelete_Click(object sender, EventArgs e) { }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtProductID.Clear();
+            txtQuantity.Clear();
+            dtpDate.Value = DateTime.Now;
+            txtSupplierID.Clear();
+        }
+    }
+}
